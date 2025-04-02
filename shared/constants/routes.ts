eff7b98fa668bc5639d115db5f58cae52ab29075
josserden <josserden@gmail.com>
2025-04-02 20:22:23 +0300

    Add routes enumeration: Define application routes for navigation

---
+++ shared/constants/routes.ts
@@ -0,0 +1,10 @@
+export enum ROUTES {
+  ABOUT = "/(app)/(authenticated)/(tabs)/profile/about",
+  ACHIEVEMENTS = "/(app)/(authenticated)/(tabs)/statistics/achievements",
+  FORGOT_PASSWORD = "/forgot-password",
+  HOME = "/",
+  LOGIN = "/login",
+  POLICY = "/(app)/(authenticated)/(tabs)/profile/policy",
+  REGISTER = "/register",
+  SETTINGS = "/(app)/(authenticated)/(tabs)/profile/settings",
+}
