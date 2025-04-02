b23d73f92982c80ef44ec5f34c89d0ee52c82511
josserden <josserden@gmail.com>
2025-04-02 22:37:44 +0300

    Add React Query support: Integrate QueryClient and QueryClientProvider in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { View } from "react-native";
 
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "@/global.css";
+import { AuthProvider } from "@/shared/context/auth-context";
+import { useAppInitialization } from "@/shared/hooks/useAppInitialization";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    "DelaGothicOne-Regular": require("../assets/fonts/Dela_Gothic_One/DelaGothicOne-Regular.ttf"),
+    "Montserrat-Bold": require("../assets/fonts/Montserrat/Montserrat-Bold.ttf"),
+    "Montserrat-LightItalic": require("../assets/fonts/Montserrat/Montserrat-LightItalic.ttf"),
+    "Montserrat-Medium": require("../assets/fonts/Montserrat/Montserrat-Medium.ttf"),
+    "Montserrat-Regular": require("../assets/fonts/Montserrat/Montserrat-Regular.ttf"),
+    "Montserrat-SemiBold": require("../assets/fonts/Montserrat/Montserrat-SemiBold.ttf"),
+  });
+
+  const { appIsReady } = useAppInitialization(loaded, error);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (appIsReady)
+    return (
+      <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+        <AuthProvider>
+          <QueryClientProvider client={queryClient}>
+            <Slot />
+          </QueryClientProvider>
+        </AuthProvider>
+      </View>
+    );
 }
