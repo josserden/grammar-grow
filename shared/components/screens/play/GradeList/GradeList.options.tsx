cd7451fd6848da949084aad04f35594d5f08744e
josserden <josserden@gmail.com>
2025-04-07 15:53:25 +0300

    feat: update grade icons in GradeList and replace Journey with specific icons

--- shared/components/screens/play/GradeList/GradeList.options.tsx
+++ shared/components/screens/play/GradeList/GradeList.options.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 
+import { Achiever } from "@/shared/components/icons/grade/Achiever";
+import { Challenger } from "@/shared/components/icons/grade/Challenger";
+import { Communicator } from "@/shared/components/icons/grade/Communicator";
+import { Explorer } from "@/shared/components/icons/grade/Explorer";
 import { Journey } from "@/shared/components/icons/grade/Journey";
+import { Learner } from "@/shared/components/icons/grade/Learner";
+import { Navigator } from "@/shared/components/icons/grade/Navigator";
+import { Proficiency } from "@/shared/components/icons/grade/Proficiency";
+import { Star } from "@/shared/components/icons/grade/Star";
+import { Strategist } from "@/shared/components/icons/grade/Strategist";
+import { Thinker } from "@/shared/components/icons/grade/Thinker";
+import { Virtuoso } from "@/shared/components/icons/grade/Virtuoso";
 import { GradeCardTestProps } from "@/shared/components/screens/play/GradeCard/GradeCard.types";
 import { GradeLevelRankEnum } from "@/shared/components/screens/play/GradeList/GradeList.types";
 import { COLORS } from "@/shared/constants/colors";
@@ -16,77 +27,77 @@ export const tests: GradeCardTestProps[] = [
   },
   {
     rank: "Rising Star",
-    icon: <Journey />,
+    icon: <Star />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.RISING_STAR}`,
     tier: 2,
     color: COLORS.BLUE_300,
   },
   {
     rank: "Proficient Learner",
-    icon: <Journey />,
+    icon: <Learner />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.PROFICIENT_LEARNER}`,
     tier: 3,
     color: COLORS.PINK_700,
   },
   {
     rank: "Skilled Navigator",
-    icon: <Journey />,
+    icon: <Navigator />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.SKILLED_NAVIGATOR}`,
     tier: 4,
     color: COLORS.YELLOW_500,
   },
   {
     rank: "Global Communicator",
-    icon: <Journey />,
+    icon: <Communicator />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.GLOBAL_COMMUNICATOR}`,
     tier: 5,
     color: COLORS.ORANGE_700,
   },
   {
     rank: "Academic Challenger",
-    icon: <Journey />,
+    icon: <Challenger />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.ACADEMIC_CHALLENGER}`,
     tier: 6,
     color: COLORS.TEAL_500,
   },
   {
     rank: "Master of Proficiency",
-    icon: <Journey />,
+    icon: <Proficiency />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.MASTER_OF_PROFICIENCY}`,
     tier: 7,
     color: COLORS.BLUE_300,
   },
   {
     rank: "Elite Thinker",
-    icon: <Journey />,
+    icon: <Thinker />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.ELITE_THINKER}`,
     tier: 8,
     color: COLORS.PINK_700,
   },
   {
     rank: "Language Virtuoso",
-    icon: <Journey />,
+    icon: <Virtuoso />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.LANGUAGE_VIRTUOSO}`,
     tier: 9,
     color: COLORS.YELLOW_500,
   },
   {
     rank: "Graduate Explorer",
-    icon: <Journey />,
+    icon: <Explorer />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.GRADUATE_EXPLORER}`,
     tier: 10,
     color: COLORS.ORANGE_700,
   },
   {
     rank: "Business Strategist",
-    icon: <Journey />,
+    icon: <Strategist />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.BUSINESS_STRATEGIST}`,
     tier: 11,
     color: COLORS.TEAL_500,
   },
   {
     rank: "Ultimate Achiever",
-    icon: <Journey />,
+    icon: <Achiever />,
     href: `${ROUTES.PLAY}/${GradeLevelRankEnum.ULTIMATE_ACHIEVER}`,
     tier: 12,
     color: COLORS.BLUE_300,
