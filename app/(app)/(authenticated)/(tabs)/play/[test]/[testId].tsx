864becf7a0b42f7db90196e013ae404d911fb4c2
josserden <josserden@gmail.com>
2025-04-07 18:38:35 +0300

    feat: enhance TestPage with circular progress indicator and update id type to string

--- app/(app)/(authenticated)/(tabs)/play/[test]/[testId].tsx
+++ app/(app)/(authenticated)/(tabs)/play/[test]/[testId].tsx
@@ -1,58 +1,59 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 
+import { ArrowDown } from "@/shared/components/icons/decor/ArrowDown";
 import { TestPage, TestPageProps } from "@/shared/components/screens/play/TestPage";
 import { Wrapper } from "@/shared/components/ui/Wrapper";
 import { COLORS } from "@/shared/constants/colors";
 
 const levels: TestPageProps[] = [
   {
-    id: 1,
+    id: "1",
     description: "This is level 1",
     color: COLORS.YELLOW_500,
   },
   {
-    id: 2,
+    id: "2",
     description: "This is level 2",
     color: COLORS.TEAL_500,
   },
   {
-    id: 3,
+    id: "3",
     description: "This is level 3",
     color: COLORS.PINK_700,
   },
   {
-    id: 4,
+    id: "4",
     description: "This is level 4",
     color: COLORS.BLUE_300,
   },
   {
-    id: 5,
+    id: "5",
     description: "This is level 5",
     color: COLORS.ORANGE_700,
   },
   {
-    id: 6,
+    id: "6",
     description: "This is level 6",
     color: COLORS.YELLOW_500,
   },
   {
-    id: 7,
+    id: "7",
     description: "This is level 7",
     color: COLORS.TEAL_500,
   },
   {
-    id: 8,
+    id: "8",
     description: "This is level 8",
     color: COLORS.PINK_700,
   },
   {
-    id: 9,
+    id: "9",
     description: "This is level 9",
     color: COLORS.BLUE_300,
   },
   {
-    id: 10,
+    id: "10",
     description: "This is level 10",
     color: COLORS.ORANGE_700,
   },
@@ -60,11 +61,23 @@ const levels: TestPageProps[] = [
 
 const TestId = () => {
   return (
-    <Wrapper alignment="start">
+    <Wrapper alignment="center" topInset={false} padding="none">
       <FlatList
+        contentContainerClassName="mt-10 gap-4"
         data={levels}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ index, item }) => <TestPage {...item} />}
+        renderItem={({ index, item }) => (
+          <View className="items-center px-4">
+            <View className="relative">
+              <TestPage {...item} />
+              <View className="absolute left-0 top-0 -z-10 h-full w-full rotate-2 rounded-[20px] bg-zinc-900" />
+            </View>
+
+            {index !== levels.length - 1 && <ArrowDown />}
+          </View>
+        )}
+        showsVerticalScrollIndicator={false}
+        refreshing
       />
     </Wrapper>
   );
--- shared/components/screens/play/TestPage.tsx
+++ shared/components/screens/play/TestPage.tsx
@@ -1,26 +1,48 @@
 import React, { FC } from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View, ViewProps } from "react-native";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
+
+import { Link } from "expo-router";
 
 import { Typography } from "@/shared/components/ui/Typography";
+import { COLORS } from "@/shared/constants/colors";
+import { ROUTES } from "@/shared/constants/routes";
 
-export interface TestPageProps {
-  id: number;
+export interface TestPageProps extends ViewProps {
+  id: string;
   description: string;
   color: string;
 }
 
-export const TestPage: FC<TestPageProps> = ({ id, description, color }) => {
+export const TestPage: FC<TestPageProps> = ({ id, description, className, color, ...props }) => {
   return (
-    <View
-      className="w-full rounded-[20px] px-[22px] py-6"
-      style={{
-        backgroundColor: color,
-      }}
-    >
-      <Typography variant="sub-title" className="mb-3.5 text-[18px]">
-        Level {id}
-      </Typography>
-      <Typography className="text-sm font-normal">{description}</Typography>
-    </View>
+    <Link href={ROUTES.PLAY} asChild>
+      <TouchableOpacity
+        className="relative w-full flex-row justify-between rounded-[20px] px-[22px] py-6"
+        style={{
+          backgroundColor: color,
+        }}
+        activeOpacity={0.85}
+        {...props}
+      >
+        <View>
+          <Typography variant="sub-title" className="mb-3.5 text-[18px]">
+            Level {id}
+          </Typography>
+          <Typography className="text-sm font-normal">{description}</Typography>
+        </View>
+
+        <AnimatedCircularProgress
+          size={74}
+          width={7}
+          fill={50}
+          tintColor={COLORS.ZINC_900}
+          backgroundColor={COLORS.WHITE}
+          rotation={0}
+        >
+          {(fill) => <Typography>{fill.toFixed(0)}%</Typography>}
+        </AnimatedCircularProgress>
+      </TouchableOpacity>
+    </Link>
   );
 };
